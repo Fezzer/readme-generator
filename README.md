ae76822b50c9bd2ce043a4de790a7517cc8ce89d
Fezzer <chris.bowles@thescreamingsheep.net>
2023-02-17 18:52:28 +0000

    prompt for license and consume answer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # readme-generator
 Bootcamp week 11 challenge
+
+
+License Badges Gist - https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 import inquirer from "inquirer";
-import { mkdir, opendir, writeFile } from "node:fs/promises";
+import { mkdir, opendir, readFile, writeFile } from "node:fs/promises";
+import inquirerPrompt from "inquirer-autocomplete-prompt";
+
+const licenses = JSON.parse(await readFile("./license-badges.json", { encoding: "utf8" }));
 
 const questions = [{
   type: "input",
   name: "title",
-  message: "What is the title of your project?"
+  message: "Enter the title of your project."
 },{
   type: "input",
   name: "description",
-  message: "Enter a description of your project?"
+  message: "Enter a description of your project."
 },{
   type: "input",
   name: "installation",
-  message: "Enter the installation instructions for your project?"
+  message: "Enter the installation instructions for your project."
 },{
   type: "input",
   name: "usage",
-  message: "Enter the usage information for your project?"
+  message: "Enter the usage information for your project."
+},{
+  type: "autocomplete",
+  name: "license",
+  message: "Select your license.",
+  source: (_, input = "") => Promise.resolve(
+    licenses
+      .filter(l => l.name.toLowerCase().includes(input.toLowerCase()))
+      .map(l => l.name))
 },{
   type: "input",
   name: "contributing",
-  message: "Enter the contribution guidelines for your project?"
+  message: "Enter the contribution guidelines for your project."
 },{
   type: "input",
   name: "tests",
-  message: "Enter the testing criteria for your project?"
+  message: "Enter the testing criteria for your project."
 }];
 
 // Temporary answers to speed up testing.
@@ -33,11 +44,14 @@ const answers = {
   description: "This is the description.",
   installation: "These are the installation instructions.",
   usage: "These are the installation instructions.",
+  license: "Apache 2.0 License",
   contributing: "These are the contribution guidelines for the project.",
   tests: "These are the testing guidelines for the project."
 };
 
-await inquirer
+inquirer.registerPrompt("autocomplete", inquirerPrompt);
+
+inquirer
   .prompt(questions, answers)
   .then(async a => {
     try {
@@ -47,17 +61,18 @@ await inquirer
       mkdir("./output");
     }
 
-    const data = `# ${a.title}\n` +
+    const data = `# ${a.title} ${licenses.find(l => l.name === a.license).badge}\n` +
       `## Table of Contents\n` +
       `- [Description](#description)\n` +
       `- [Installation](#installation)\n` +
       `- [Usage](#usage)\n` +
+      `- [License](#license)\n` +
       `- [Contributing](#contributing)\n` +
       `- [Tests](#tests)\n` +
       `## Description\n${a.description}\n` +
       `## Installation\n${a.installation}\n` +
       `## Usage\n${a.usage}\n` +
-      //`## License\n${answers.license}\n` +
+      `## License\nThis repo is licensed under ${a.license}.\n` +
       `## Contributing\n${a.contributing}\n` +
       `## Tests\n${a.tests}`;
 
