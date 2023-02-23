c4bf0ce165466c34958d745ca992b5a5c2baee6b
Fezzer <chris.bowles@thescreamingsheep.net>
2023-02-23 17:56:14 +0000

    allow use of default answers from the command line arguments

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import { defaultAnswers } from "./defaultAnswers.js";
 import { getQuestions } from "./questions.js";
 import { createReadMeText, getLicensesAsync, writeReadMeAsync } from "./helpers.js";
 
+const useDefaultAnswers = process.argv[2];
 const licenses = await getLicensesAsync();
 const questions = getQuestions(licenses);
 
 inquirer.registerPrompt("autocomplete", inquirerPrompt);
 
 const answers = await inquirer
-  .prompt(questions, defaultAnswers)
+  .prompt(questions, useDefaultAnswers ? defaultAnswers : null)
   .then(answers => answers);
   
 const data = createReadMeText(answers, licenses);
